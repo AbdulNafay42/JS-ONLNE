d4bbf3b6c2a61126430b574cbb66d1a2bd6504cc
Abdul Nafay <103064913+AbdulNafay42@users.noreply.github.com>
2023-04-12 01:29:19 +0500

    Add files via upload

---
+++ Assigment-1/app.js
@@ -0,0 +1,89 @@
+// var age = prompt("Enter your age")
+// if(age <= 10){
+//     alert("Welcome!")
+// }
+// else(age > 10)
+//     alert("Yor are not allowed")
+
+// Question # 1
+
+// var num1 = prompt("Enter your first number");
+// var num2 = prompt("Enter your Second number");
+
+
+// if(num1 > num2){
+//     console.log("The Biggest value is: " + num1 )
+
+// }
+// else if(num2 > num1){
+//     console.log("The Biggest value is: " + num2 )
+
+// }
+
+// Question # 2
+
+
+// let number = prompt("enter the number")
+// if(number > 0 ){
+//     alert("the sign is +")
+// }
+// else if(number < 0){
+//     alert("the sign is -")
+
+// }
+
+// else{
+//     alert("the sign is zero")
+
+// Question # 3
+
+// let num1 = prompt("Enter the first number");
+// let num2 = prompt("Enter the Second number");
+// let num3 = prompt("Enter the Third number");
+// let num4 = prompt("Enter the fourth number");
+// let num5 = prompt("Enter the fifth number");
+
+
+// if(num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5 ){
+// console.log(num1);}
+
+// else if(num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5){
+// console.log(num2);}
+
+// else if(num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5){
+// console.log(num3);
+// }
+
+// else if(num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5){
+//     console.log(num4);
+//     }
+
+//     else {
+//         console.log(num5);
+//         }
+
+// Question # 4
+
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i + ' is even');
+//     } else {
+//       console.log(i + ' is odd');
+//     }
+//   }
+
+// Question # 7
+
+// for (let i = 1; i <= 5; i++) {
+// let pattern = ' '
+// for (let j = 1; j <= i; j++) {
+//     pattern += "*";
+//   }
+//   console.log(pattern)
+
+// }
+
+clet students
+
+
+
